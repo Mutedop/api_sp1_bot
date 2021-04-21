1997431127d7ebb500bea64428080ef5d58a1905
Pavel Panchenko <mutedop@yandex.ru>
2021-04-21 09:35:23 +0800

    "At zero time value comes project 5"

--- homework.py
+++ homework.py
@@ -6,31 +6,31 @@ import telegram
 import logging
 
 from dotenv import load_dotenv
-from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
-# здесь мы задаем глобальную конфигурацию для всех логеров
+# noinspection PyArgumentList
 logging.basicConfig(
     level=logging.DEBUG,
     filename='program.log',
+    filemode='w',
+    encoding='UTF-8',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
-# а тут настраиваем логгер для текущего файла .py
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler('my_logger.log',
-                              maxBytes=50000000,
-                              backupCount=5)
-logger.addHandler(handler)
+
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-HOMEWORKS_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+URL_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
+    """Compare json string response with actual status,
+    prepared messages under bot output.
+    """
+
+    logging.debug(msg='Запуск парсера.')
     homework_name = homework.get('homework_name')
     status = homework.get('status')
     if status == 'rejected':
@@ -46,45 +46,45 @@ def parse_homework_status(homework):
 def get_homework_statuses(current_timestamp):
     """Responding to a Well-formed API Request Praktikum."""
 
-    ...  # что тут ожидается?
-    if current_timestamp is None:
-        current_timestamp = int(time.time())
     try:
         homework_statuses = requests.get(
-            HOMEWORKS_API,
+            URL_API,
             params={'from_date': current_timestamp},
-            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'})
+            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+        )
         return homework_statuses.json()
     except requests.RequestException as error:
-        logger.error(
+        logging.error(
             f'Не правильно сформированный запрос API. Ошибка: {error}'
         )
         return {}
 
 
 def send_message(message, bot_client):
-    ...
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    logging.debug(msg='| Запуск бота |')
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0])
+                    parse_homework_status(
+                        new_homework.get('homeworks')[0]), bot
                 )
+                logging.info('| Сообщение отправлено. |')
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp
-            )  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            )
+            time.sleep(300)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+            logging.error(f'Бот столкнулся с ошибкой: {e}')
             time.sleep(5)
 
 
